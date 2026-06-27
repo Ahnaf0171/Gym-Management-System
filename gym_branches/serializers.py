2c8ddd1cfd9fbffee9aa6af362a5da6274b52df1
Abdullah Ahnaf <99898957+Ahnaf0171@users.noreply.github.com>
2026-06-27 14:49:33 +0600

    new branches api added(public)

--- gym_branches/serializers.py
+++ gym_branches/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from .models import GymBranch
+from django.db.models import Prefetch
+from accounts.models import User
 
 class GymBranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = GymBranch
         fields = ["id", "name", "location", "created_at"]
         read_only_fields = ["id", "created_at"]
+
+
+class PublicGymBranchSerializer(serializers.ModelSerializer):
+    manager_email = serializers.SerializerMethodField()
+    manager_phone = serializers.SerializerMethodField()
+
+    class Meta:
+        model = GymBranch
+        fields = ["id", "name", "location", "manager_email", "manager_phone"]
+
+    def get_manager_email(self, obj):
+        managers = getattr(obj, 'managers', [])
+        return managers[0].email if managers else None
+
+    def get_manager_phone(self, obj):
+        managers = getattr(obj, 'managers', [])
+        return managers[0].mobile_number if managers else None
--- gym_branches/views.py
+++ gym_branches/views.py
@@ -1,28 +1,51 @@
 from rest_framework import viewsets, mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.filters import SearchFilter
+from django.db.models import Prefetch
 from accounts.permissions import role_required
+from accounts.models import User
 from .models import GymBranch
-from .serializers import GymBranchSerializer
-from accounts.models import User  
-from accounts.permissions import role_required  
-from rest_framework.filters import SearchFilter   
+from .serializers import GymBranchSerializer, PublicGymBranchSerializer
+
 
 class GymBranchViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,     
-    mixins.DestroyModelMixin,     
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
     serializer_class = GymBranchSerializer
-    filter_backends = [SearchFilter]                  
-    search_fields = ["name", "location"]              
+    filter_backends = [SearchFilter]
+    search_fields = ["name", "location"]
 
     def get_queryset(self):
-        return GymBranch.objects.all()
+        return GymBranch.objects.prefetch_related(
+            Prefetch(
+                'users',
+                queryset=User.objects.filter(role=User.MANAGER),
+                to_attr='managers'
+            )
+        ).all()
 
     def get_permissions(self):
+        if self.action == "public_list":
+            return [AllowAny()]
         if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated(), role_required(User.SUPER_ADMIN)()]
         return [IsAuthenticated(), role_required(User.SUPER_ADMIN, User.MANAGER)()]
+
+    @action(detail=False, methods=['get'], permission_classes=[AllowAny], url_path='public')
+    def public_list(self, request):
+        queryset = GymBranch.objects.prefetch_related(
+            Prefetch(
+                'users',
+                queryset=User.objects.filter(role=User.MANAGER),
+                to_attr='managers'
+            )
+        )
+        serializer = PublicGymBranchSerializer(queryset, many=True)
+        return Response(serializer.data)
